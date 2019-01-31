f39e0fa7a1cbe8e4aaffd279d604d5387a36563f
Synaxis <oiacow@gmail.com>
2019-01-31 18:58:41 -0200

    arrays

---
+++ ConditionalArr, Vec, Str, Loop/Arrays/main.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <limits>
+
+int main() {
+
+    int arrNums[10] = {1}; //set a default value if you want
+
+    int arrNums2[] = {1,2,3}; // auto define the size of the array
+
+    int arrNums3[5] = {8,9}; // the last index will have 8 9 vals
+
+    std::cout << "1st value of this array : " << arrNums3[0] << "\n";
+    arrNums3[0] = 7;
+    std::cout << "1st value of this array before :" << arrNums3[0] << "\n";
+
+    std::cout << "Array size : " <<  sizeof(arrNums3) / sizeof(*arrNums3) << "\n";
+    return 0;
+}
