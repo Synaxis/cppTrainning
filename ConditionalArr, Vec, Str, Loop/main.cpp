bfb833c925f8597077b585ca3fed7ddc6bcfe41f
Synaxis <oiacow@gmail.com>
2019-01-31 18:10:38 -0200

    fixed weird syntax

--- ConditionalArr, Vec, Str, Loop/main.cpp
+++ ConditionalArr, Vec, Str, Loop/main.cpp
@@ -1,21 +1,22 @@
 #include <vector>
 #include <sstream>
 #include <limits>
-
+#include <iostream>
 int main() {
-    std::string sAge = 0;
+    std::string sAge = "0";
     std::cout << "Enter your age : ";
+    getline(std::cin, sAge);
     int nAge = std::stoi(sAge); // like strconv
 
-    if ((nAge >= 1)) && (nAge <= 18)){
+    if ((nAge >= 1) && (nAge <= 18)){
         std::cout << "Important Birthday\n";
-    } else if ((nAge == 21)) || (nAge == 50)) {
+    } else if ((nAge == 21) || (nAge == 50)) {
         std::cout << "Someone else important bday\n";
     } else if (nAge >= 65){
         std::cout << "Important day\n";
     }
     else {
-        std::cout "Not an important day\n";
+        std::cout << "Not an important day\n";
     }
 
     return 0;
