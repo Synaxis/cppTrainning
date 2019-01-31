11e3fe27b154adcb268ef53dffc74e75ee05b682
Synaxis <oiacow@gmail.com>
2019-01-31 19:40:22 -0200

    complex arrays

--- ConditionalArr, Vec, Str, Loop/Arrays/main.cpp
+++ ConditionalArr, Vec, Str, Loop/Arrays/main.cpp
@@ -18,5 +18,13 @@ int main() {
     std::cout << "1st value of this array before :" << arrNums3[0] << "\n";
 
     std::cout << "Array size : " <<  sizeof(arrNums3) / sizeof(*arrNums3) << "\n";
+    
+    //int arrNums4[2][2][2] //pages //collumns //rows
+    int arrNums4[2][2][2] = {{{1,2}, {3,4}},
+    {{5,6}, {7,8}}}; 
+
+    std::cout << arrNums4[1][1][1] << "\n";
+
+
     return 0;
 }
