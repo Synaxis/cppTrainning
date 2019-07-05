4b6e8176a7c29024e54dce1fa942bf030fe9b16c
Synaxis <oiacow@gmail.com>
2019-07-04 23:24:33 -0300

    test todo

---
+++ 2019/6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int num = 10;
+  cout << num << "\n"; // print original value
+  num = num * -1; //invert original value
+  cout << num << "\n"; //will print -10
+
+  return 0;
+}
+
+  
